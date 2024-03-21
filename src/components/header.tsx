5d43ae4fa7a709787b06267376eb4107fc901d4f
jwhubert91 <jwhubert91@gmail.com>
2024-03-21 00:00:58 -0500

    remove chakra, add tailwind and daisy ui

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">OnlyDevs</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
